fdbe87b8dfb329e990c71bcb56fca95df2b07d87
DaGiBi <najibaseri@gmail.com>
2023-03-17 19:45:17 +0800

    add controller and routes files

--- server/index.js
+++ server/index.js
@@ -9,7 +9,15 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { error } from "console";
+
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js";
+import postRoutes from  "./routes/post.js";
 import { register } from "./controller/auth.js";
+import { verifyToken } from "./middleware/auth.js";
+import { createPost } from "./controller.posts.js";
+
+
 
 /*configuration*/
 const __filename = fileURLToPath(import.meta.url);
@@ -41,10 +49,17 @@ const upload = multer({storage});
 
 /* Routes with file */
 
-app.post("/auth/register", upload.single("picture"),register); // upload fucntion perforem save image into stroagre
+app.post("/auth/register", upload.single("picture"), register); // upload fucntion perforem save image into stroagre
+app.post("/post", verifyToken, upload.single("picture"), createPost); 
+
+// Routes
+app.use("/auth", authRoutes);
+app.user("/users", userRoutes);
+app.user("/posts", postRoutes);
+
 
 /* MONGOOSE SETUP */
-dotenv.config({path:'.env'})
+// dotenv.config({path:'.env'})
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
     useNewURLParser: true,
---
+++ server/routes/user.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+    getUser,
+    getUserFriends,
+    addRemoveFriend,
+} from "../controller/user.js";
+import { verifyToken } from "../middleware/auth.js";;
+
+const router = express.Router();;
+
+/* Read */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id", verifyToken, getUserFriends);
+
+/* Updatre */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+export default router;;
