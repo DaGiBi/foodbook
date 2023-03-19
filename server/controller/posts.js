cbcaa203a0f9d877596a1fcdaf0ac7070992bf42
DaGiBi <najibaseri@gmail.com>
2023-03-19 23:27:01 +0800

    add post controller, model

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -1,2 +1,27 @@
 import Post from "../models/Posts.js";
 
+// create
+export const createPost = async (req, res) => {
+    try{
+        const { userId, desciption, pitcurePath } = req.body;
+        const user = await user.findById(userId);
+        const newPost = new Post({
+            userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            location: user.location,
+            desciption,
+            userPitcurePath: user.pitcurePath,
+            pitcurePath,
+            likes: {},
+            comments: []
+        })
+
+        await newPost.save();
+
+        const post = await Post.find();
+        res.status(201).json(post);
+    } catch (err){
+        res.status(409).json({ message: err.message})
+    }
+}
